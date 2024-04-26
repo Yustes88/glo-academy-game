a6fab764bf157891b948de17ae2da844787c194d
Yustes88 <iuliiaobr.io@gmail.com>
2024-04-26 21:41:39 +0300

    added attempts, still to fix

--- script.js
+++ script.js
@@ -1,31 +1,64 @@
-function isNum(num) {
-  return !isNaN(parseFloat(num)) && isFinite(num);
+let attempts = 10
+
+const isNum = function (num) {
+  return !isNaN(parseFloat(num)) && isFinite(num)
 }
 
-function guessUserNumber() {
-  const secretNumber = 88;
-  const userNum = prompt("Угадай число от 1 до 100");
+const randomIntFromInterval = function (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const randomInteger = randomIntFromInterval(1, 100)
+
+const guessUserNumber = function () {
+  let secretNumber = 88
+  let userNum = prompt('Угадай число от 1 до 100')
 
   if (userNum === null) {
-    alert("Игра закончена");
-    return;
+    alert('Игра закончена')
+    return
+  }
+
+  const checkAttempts = function () {
+    if (attempts === 0) {
+      const answer = confirm('Попытки закончились, хотите сыграть еще?')
+
+      if (answer) {
+        attempts = 10
+        guessUserNumber()
+      } else {
+        alert('Игра закончена')
+        return
+      }
+    }
   }
 
   if (isNum(userNum)) {
-    userNum = +userNum;
+    userNum = +userNum
     if (userNum > secretNumber) {
-      alert("Загаданное число меньше");
+      attempts--
+      alert(`Загаданное число меньше, осталось попыток: ${attempts}`)
+      checkAttempts()
     } else if (userNum < secretNumber) {
-      alert("Загаданное число больше");
+      attempts--
+      alert(`Загаданное число больше, осталось попыток: ${attempts}`)
+      checkAttempts()
     } else if (userNum === secretNumber) {
-      alert("Поздравляю, Вы угадали!!!");
-      return userNum;
+      const answer = confirm('Поздравляю, Вы угадали!!! Хотели бы сыграть еще?')
+
+      if (answer) {
+        attempts = 10
+        secretNumber = randomIntFromInterval(1, 100)
+        guessUserNumber()
+      }
+      alert('Игра закончена')
+      return
     }
   } else {
-    alert("Введи число");
+    alert('Введи число')
   }
 
-  return guessUserNumber();
+  return guessUserNumber()
 }
 
-console.log(guessUserNumber());
+console.log(guessUserNumber())
