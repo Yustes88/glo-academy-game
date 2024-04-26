cb8f5819252e5a3c4a8423b96a474457f55ca331
Iuliia <iuliiaobr.io@gmail.com>
2024-04-26 21:04:27 +0700

    displayed messages based on users input

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Hello, world!");
+function isNum(num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+}
+
+function guessUserNumber() {
+  const secretNumber = 88;
+  const userNum = prompt("Угадай число от 1 до 100");
+
+  if (userNum === null) {
+    alert("Игра закончена");
+    return;
+  }
+
+  if (isNum(userNum)) {
+    userNum = +userNum;
+    if (userNum > secretNumber) {
+      alert("Загаданное число меньше");
+    } else if (userNum < secretNumber) {
+      alert("Загаданное число больше");
+    } else if (userNum === secretNumber) {
+      alert("Поздравляю, Вы угадали!!!");
+      return userNum;
+    }
+  } else {
+    alert("Введи число");
+  }
+
+  return guessUserNumber();
+}
+
+console.log(guessUserNumber());
