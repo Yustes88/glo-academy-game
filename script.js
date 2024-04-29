0f2f184ab57bcb2d31d7c9ede66c2347d69907c2
Yustes88 <iuliiaobr.io@gmail.com>
2024-04-29 15:26:48 +0300

    fixed bugs

--- script.js
+++ script.js
@@ -1,62 +1,67 @@
-let attempts = 10;
-
 const isNum = function (num) {
-  return !isNaN(parseFloat(num)) && isFinite(num);
-};
+  return !isNaN(parseFloat(num)) && isFinite(num)
+}
 
 const randomIntFromInterval = function (min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
 const guessUserNumber = function () {
-  let secretNumber = 88;
-  let userNum = prompt("Угадай число от 1 до 100");
+  let secretNumber = randomIntFromInterval(1, 100)
+  let attempts = 10
 
-  if (userNum === null) {
-    alert("Игра закончена");
-    return;
+  const finish = function () {
+    alert('Игра закончена')
+    return
   }
 
-  const checkAttempts = function () {
-    if (attempts === 0) {
-      const answer = confirm("Попытки закончились, хотите сыграть еще?");
+  const round = function () {
+    let userNum = prompt('Угадай число от 1 до 100')
+
+    if (attempts === 1 && secretNumber !== userNum) {
+      const answer = confirm('Попытки закончились, хотите сыграть еще?')
 
       if (answer) {
-        attempts = 10;
-        guessUserNumber();
+        attempts = 10
+        round()
       } else {
-        alert("Игра закончена");
-        return;
+        finish()
+        return
       }
     }
-  };
 
-  if (isNum(userNum)) {
-    userNum = +userNum;
-    if (userNum > secretNumber) {
-      attempts--;
-      alert(`Загаданное число меньше, осталось попыток: ${attempts}`);
-      checkAttempts();
-    } else if (userNum < secretNumber) {
-      attempts--;
-      alert(`Загаданное число больше, осталось попыток: ${attempts}`);
-      checkAttempts();
-    } else if (userNum === secretNumber) {
-      const answer = confirm(
-        "Поздравляю, Вы угадали!!! Хотели бы сыграть еще?"
-      );
+    if (userNum === null) {
+      finish()
+    } else if (!isNum(userNum)) {
+      alert('Введи число')
+      round()
+    } else if (isNum(userNum)) {
+      userNum = +userNum
+      if (userNum > secretNumber) {
+        attempts--
+        alert(`Загаданное число меньше, осталось попыток: ${attempts}`)
+        round()
+      } else if (userNum < secretNumber) {
+        attempts--
+        alert(`Загаданное число больше, осталось попыток: ${attempts}`)
+        round()
+      } else if (userNum === secretNumber) {
+        const answer = confirm(
+          'Поздравляю, Вы угадали!!! Хотели бы сыграть еще?',
+        )
 
-      if (answer) {
-        attempts = 10;
-        secretNumber = randomIntFromInterval(1, 100);
-        guessUserNumber();
+        if (answer) {
+          attempts = 10
+          secretNumber = randomIntFromInterval(1, 100)
+          round()
+        } else {
+          finish()
+        }
       }
-      alert("Игра закончена");
-      return;
     }
-  } else {
-    alert("Введи число");
   }
 
-  return guessUserNumber();
-};
+  round()
+}
+
+guessUserNumber()
